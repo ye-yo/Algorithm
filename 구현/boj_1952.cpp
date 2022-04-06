5e3c3b3b5639c533c10dd8815a5e0dc2bb24db02
ye-yo <yeyo0x0@gmail.com>
2022-04-06 18:09:31 +0900

    BOJ [1952]

---
+++ 구현/boj_1952.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+#define MAX 100
+
+int M, N;
+int arr[MAX][MAX] = {0,};
+int cornerCount = 0;
+
+struct Position {
+  int x;
+  int y;
+  int direction; //우: 1, 하 : 2, 좌 : 3, 상 : 4
+};
+
+Position getNextPosition(Position pos){
+  switch (pos.direction){
+    case 1:
+      pos.y++;
+      break;
+    case 2:
+      pos.x++;
+      break;
+    case 3:
+      pos.y--;
+      break;
+    case 4:
+      pos.x--;
+    break;
+    default:break;
+  }
+  return pos;
+}
+
+Position move(Position pos)
+{
+  Position temp = {pos.x, pos.y, pos.direction};
+  temp = getNextPosition(temp);
+  int prevX = temp.x;
+  int prevY = temp.y;
+
+  if(prevX >= M || prevY >= N || prevX < 0 || prevY < 0 || arr[prevX][prevY] > 0){
+    cornerCount++;
+    pos.direction = pos.direction >= 4 ? 1 : ++pos.direction;
+    pos = getNextPosition(pos);
+    return pos;
+  }
+  return temp;
+}
+
+int main(void)
+{
+  cin >> M >> N;
+
+  int i = 0;
+  int j = 0;
+  int emptyCell = M * N;
+  Position pos = {0, 0, 1};
+  while (true)
+  {
+    arr[pos.x][pos.y] = 1;
+    emptyCell--;
+    pos = move(pos);
+    if (emptyCell == 1)
+        break;
+  }
+  printf("%d\n", cornerCount);
+  return 0;
+}
